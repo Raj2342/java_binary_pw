34e39d77405bba5d92a59034c3247bc52f67f6e4
Himanshu Gupta <technologys411@gmail.com>
2024-03-11 15:40:52 +0530

    comman_element_array_question

---
+++ striver_ninja/array/common_ele_two_Array/common_ele.java
@@ -0,0 +1,35 @@
+ class student {
+
+     public void  mohan(int a[] ){
+    
+        for(int i=0; i<a.length;i++){
+             if (a[i]==a[i+1])
+              {
+                  System.out.println(a[i]);
+              } 
+        }
+            
+      
+        }
+      
+   
+ }
+ 
+ 
+ 
+ class common_ele {
+   
+    public static void main(String[] args) {
+        
+            int a[] = {  4,3,7,9,2
+                        ,5,1,4,8,3 
+                       };
+          
+
+            
+
+            
+
+
+    }
+ }
