6b12f1d28c1ad60b1bb9f3bdbd5e4e4a095a7a5a
RAJ <technologys411@gmail.com>
2024-04-02 20:49:33 +0530

    REVISON -inheritance , polymorphism , abstraction , interface and lambda , exception handling

---
+++ exception handling/rough.java
@@ -0,0 +1,10 @@
+public class rough {
+    public static void main(String[] args) {
+        
+
+                System.out.println(10/0);
+            
+           
+            System.out.println("HEY");
+    }
+}
