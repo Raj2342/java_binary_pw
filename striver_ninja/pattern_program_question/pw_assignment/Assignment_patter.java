f419617849abd5dff201886c7b6f1582acd2cebf
RAJ <technologys411@gmail.com>
2024-04-05 18:36:23 +0530

    pattern programming practice and revise loop concept

---
+++ striver_ninja/pattern_program_question/pw_assignment/Assignment_patter.java
@@ -0,0 +1,123 @@
+public class  Assignment_patter {
+    public static void main(String[] args) {
+      
+      //  concept-1
+      // *****
+      // *****
+      // *****
+      // *****
+      // *****
+      //  for(int i=0; i<5;i++)
+      //  {
+      //     for(int j=0; j<5; j++)
+      //     {
+      //       System.out.print("*");
+      //     }
+      //     System.out.println();
+      //  }
+
+      //   concept-2
+      // *****
+      // *   *
+      // *   *
+      // *   *
+      // *****
+      // int n =5;
+      // for(int i=0; i<5;i++)
+      // {
+      //    for(int j=0; j<5; j++)
+      //    {
+      //       if (i==0 || i==(n-1) || j==0 || j==(n-1) ) {
+               
+      //          System.out.print("*");
+      //       }
+      //       else{
+      //          System.out.print(" ");
+      //       }
+      //    }
+      //    System.out.println();
+      // }
+       
+   //   concept-3
+   // *****
+   // *   *
+   // *****
+   // *   *
+   // *   *
+   //     int n=5;
+   //  for(int i=0;i<n;i++)
+   //  {
+   //    for(int j=0; j<n;j++)
+   //    {
+   //      if (j==0 || j==(n-1) || i==0 || i==(n-1)/2) {
+         
+   //         System.out.print("*");
+   //      }
+   //      else{
+   //       System.out.print(" ");
+   //      }
+   //    }
+    
+   //    System.out.println();
+   //  }
+
+   /***********************************************************************
+                     PW ASSIGNMENT PATTERN BASED QUESTION 
+
+    **********************************************************************/
+   // Q-1 DRAW A 
+         // int n=5;
+         //  for(int i=0;i<n;i++)
+         //  {
+         //    for(int j=0; j<n;j++)
+         //    {
+         //      if (i==0  || i==(n-1)/2 || j==0 || j==(n-1) ) {
+               
+         //         System.out.print("* ");
+         //      }
+         //      else{
+         //       System.out.print("  ");
+         //      }
+         //    }
+          
+         //    System.out.println();
+         //  }
+
+      // q-2: draw B
+      
+      int n=5;
+          for(int i=0;i<n;i++)
+          {
+            for(int j=0; j<n;j++)
+            {
+              if (i==0 || i==(n-1) || i==(n-1)/2 || j==0 || j==(n-1) ) {
+               
+                 System.out.print("* ");
+              }
+              else{
+               System.out.print("  ");
+              }
+            }
+          
+            System.out.println();
+          }
+       
+        
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   }
+}
