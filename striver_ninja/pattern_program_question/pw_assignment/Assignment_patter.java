578be1a1358e532b39d9d58752cae3819e0123fb
RAJ <technologys411@gmail.com>
2024-04-05 23:05:01 +0530

    pattern programming

--- striver_ninja/pattern_program_question/pw_assignment/Assignment_patter.java
+++ striver_ninja/pattern_program_question/pw_assignment/Assignment_patter.java
@@ -84,31 +84,128 @@ public class  Assignment_patter {
          //  }
 
       // q-2: draw B
-      
-      int n=5;
-          for(int i=0;i<n;i++)
-          {
-            for(int j=0; j<n;j++)
-            {
-              if (i==0 || i==(n-1) || i==(n-1)/2 || j==0 || j==(n-1) ) {
+      // * * * *   
+      // *       *
+      // * * * * *
+      // *       *
+      // * * * *
+      // int n=5;
+      //     for(int i=0;i<n;i++)
+      //     {
+      //       for(int j=0; j<n;j++)
+      //       {
+      //         if (i==0 && j<(n-1) || i==(n-1) &&  j<(n-1)|| i==(n-1)/2  || j==0  || j==(n-1) && i>0 && i!=(n-1)) {
                
-                 System.out.print("* ");
-              }
-              else{
-               System.out.print("  ");
-              }
-            }
+      //            System.out.print("* ");
+      //         }
+      //         else{
+      //          System.out.print("  ");
+      //         }
+      //       }
           
-            System.out.println();
-          }
+      //       System.out.println();
+      //     }
+
+      // Q-3: c
+      //   * * * * 
+      // *
+      // *
+      // *
+      //   * * * *      
+                                        
+      // int n=5;
+      // for(int i=0;i<n;i++)
+      // {
+      //   for(int j=0; j<n;j++)
+      //   {
+      //     if (i==0 && j!=0 || j==0 && i>0 && i<(n-1) || i==(n-1) && j!=0 ) {
+           
+      //        System.out.print("* ");
+      //     }
+      //     else{
+      //      System.out.print("  ");
+      //     }
+      //   }
+      
+      //   System.out.println();
+      // }
        
-        
+      // Q-4: D  
+      // * * * *   
+      // *       *
+      // *       *
+      // *       *
+      // * * * *        
+      // int n=5;
+      // for(int i=0;i<n;i++)
+      // {
+      //   for(int j=0; j<n;j++)
+      //   {
+      //     if (j==0 || j==(n-1) && i>0 && i<(n-1) || i==0 && j!=(n-1) || i==(n-1) && j!=(n-1) ) {
+           
+      //        System.out.print("* ");
+      //     }
+      //     else{
+      //      System.out.print("  ");
+      //     }
+      //   }
+      
+      //   System.out.println();
+      // }
 
-
+    // Q-5: E 
+    // int n=5;
+    // for(int i=0;i<n;i++)
+    // {
+    //   for(int j=0; j<n;j++)
+    //   {
+    //     if(j==0 || i==0 || i==(n-1)/2 ||  i==(n-1)  ) {
+         
+    //        System.out.print("* ");
+    //     }
+    //     else{
+    //      System.out.print("  ");
+    //     }
+    //   }
+    
+    //   System.out.println();
+    // }
    
+  //  Q-6: F
+  // int n=5;
+  // for(int i=0;i<n;i++)
+  // {
+  //   for(int j=0; j<n;j++)
+  //   {
+  //     if(j==0 || i==0 || i==(n-1)/2   ) {
+       
+  //        System.out.print("* ");
+  //     }
+  //     else{
+  //      System.out.print("  ");
+  //     }
+  //   }
+  
+  //   System.out.println();
+  // }
    
-   
-   
+  //  Q-7: G
+  int n=5;
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0; j<n;j++)
+    {
+      if(j==0 || i==(n-1)  ) {
+       
+         System.out.print("* ");
+      }
+      else{
+       System.out.print("  ");
+      }
+    }
+  
+    System.out.println();
+  }
    
    
    
