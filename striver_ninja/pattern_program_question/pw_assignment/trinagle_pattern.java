8440f2181d57ee543a8ccdc29c14bf11dd7dc33e
RAJ <technologys411@gmail.com>
2024-04-08 18:43:37 +0530

    pattern q.

--- striver_ninja/pattern_program_question/pw_assignment/trinagle_pattern.java
+++ striver_ninja/pattern_program_question/pw_assignment/trinagle_pattern.java
@@ -78,25 +78,150 @@ public class trinagle_pattern {
     // observation : j<=6-i
 
 //       concept-4
-  int n=4;
-  for(int i=1; i<=n; i++)
-  {
-    for(int j=1; j<=(2*n-1) ;j++)
-    {
-        if (j>=(n+1)-i && j<=(n-1)+i) {
-            System.out.print("*");
+                    //       *   
+                    //      ***
+                    //     *****
+                    //    *******
+//   int n=4;
+//   for(int i=1; i<=n; i++)
+//   {
+//     for(int j=1; j<=(2*n-1) ;j++)
+//     {
+//         if (j>=(n+1)-i && j<=(n-1)+i) {
+//             System.out.print("*");
              
-        }
-        else{
-            System.out.print(" ");
-        }
-    }
-    System.out.println();
+//         }
+//         else{
+//             System.out.print(" ");
+//         }
+//     }
+//     System.out.println();
+//   }
+
+//    concept-5
+
+// complex structure
+// * * * * * 
+// * *   * *
+// *   *   *
+// * *   * *
+// * * * * *
+  
+// int n=5;
+// for(int i=0; i<n; i++)
+// {
+//   for(int j=0; j<n ;j++)
+//   {
+//       if (i==0 || i==(n-1) || j==0 || j==(n-1) || i==j || i+j==(n-1)) {
+//           System.out.print("* ");
+           
+//       }
+//       else{
+//           System.out.print("  ");
+//       }
+//   }
+//   System.out.println();
+// }
+
+//   concept-6
+// * * * * * * * * * * 
+// * * * *   * * * * *
+// * * *       * * * *
+// * *           * * *
+// *               * *
+// * *             * *
+// * * *         * * *
+// * * * *     * * * *
+// * * * * * * * * * *
+// * * * * * * * * * *
+// int n=10;
+// for(int i=0; i<n; i++)
+// {
+//   for(int j=0; j<n ;j++)
+//   {
+//       if (i==0 || i==(n-1) || j==0 || j==(n-1) || i+j<=(n-1)/2  ||i-j>=(n-1)/2 ||j-i>=(n-1)/2 || j+i>=(n-1)+(n-1)/2 ) {
+//           System.out.print("* ");
+           
+//       }
+//       else{
+//           System.out.print("  ");
+//       }
+//   }
+//   System.out.println();
+// }
+
+
+    //   concept-11
+    // * * * * * * * * * * 
+    // *     * * *       *
+    // *   * * * * *     *
+    // * * * * * * * *   *
+    // * * * * * * * * * *
+    // * * * * * * * * * *
+    // *   * * * * * *   *
+    // *     * * * *     *
+    // *       * *       *
+    // * * * * * * * * * *
+//     int n=10;
+// for(int i=0; i<n; i++)
+// {
+//   for(int j=0; j<n ;j++)
+//   {
+//       if (i==0 || i==(n-1) || j==0 || j==(n-1) || i+j>=(n-1)/2 &&  i<=(n-1)/2 &&  j-i<=(n-1)/2  ||i-j<=(n-1)/2 && i>=(n-1)/2 && j+i<=(n-1)+(n-1)/2 ||j-i==(n-1)/2 || j+i==(n-1)+(n-1)/2 ) {
+//           System.out.print("* ");
+           
+//       }
+//       else{
+//           System.out.print("  ");
+//       }
+//   }
+//   System.out.println();
+// }
+
+
+    // concept-12
+//     int n=10;
+// for(int i=0; i<n; i++)
+// {
+//   for(int j=0; j<n ;j++)
+//   {
+//       if (j==0 || j==(n-1) || i==(n-1) || ) {
+//           System.out.print("* ");
+           
+//       }
+//       else{
+//           System.out.print("  ");
+//       }
+//   }
+//   System.out.println();
+// }
+
+// concept-13
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+int n=5;
+for(int i=1; i<=n; i++)
+{
+  for(int j=1; j<=2*n ;j++)
+  {
+      if (j<=i || j>=(2*n+1)-i  ) {
+          System.out.print("*");
+           
+      }
+      else{
+          System.out.print(" ");
+      }
   }
+  
+  
+  System.out.println();
+}
 
 
 
-
-
+        
     }
 }
