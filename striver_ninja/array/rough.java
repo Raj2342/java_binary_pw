bfa99d9cc4d424de23867ee76d33dfd1be80da5d
RAJ <technologys411@gmail.com>
2024-04-12 23:22:14 +0530

    arrays based question practice

--- striver_ninja/array/rough.java
+++ striver_ninja/array/rough.java
@@ -1,28 +1,16 @@
 import java.util.Arrays;
 
 public class rough {
-        public static int  play(int a[]){
-            
-            int temp;
-              for(int i=0; i<a.length-1;i++)
-              {
-                for(int j=0;j<a.length-1-i;j++)
-                {
-                    if (a[j]<a[j+1]) {
-                        temp=a[j+1];
-                        a[j+1]=a[j];
-                        a[j]=temp;
-                    }
-                }
-              }
-              return a[a.length-2];
-        }
+      
     public static void main(String[] args) {
-         
-            int a[] = { 4,3,5,2,1,6};
-            play(a);
-            System.out.println(Arrays.toString(a));
-            System.out.println(play(a));
+         int a[] = {1,2,2,3,4,4,5};
+         for(int i=0;i<a.length-1;i++)
+         {
+            if (a[i]==a[i+1]) {
+                System.out.println(a[i]);
+            }
+         }
+           
 
     }
 }
