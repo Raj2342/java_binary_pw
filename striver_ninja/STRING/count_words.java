d6a3e667a1570d8f468e13123131506fb68e7f93
RAJ <technologys411@gmail.com>
2024-04-13 23:42:36 +0530

    COUNT WORDS

---
+++ striver_ninja/STRING/count_words.java
@@ -0,0 +1,10 @@
+public class count_words {
+    public static void main(String[] args) {
+        String  s = "play ground";
+        for(int i=0; i<s.length();i++)
+        {
+           System.out.println(s.charAt(i));
+        }
+
+    }
+}
