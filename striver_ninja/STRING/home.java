18cbf12cb02fdd5850c3a001fdc0a1a3c27be906
RAJ <technologys411@gmail.com>
2024-04-16 23:28:47 +0530

    count word in string

--- striver_ninja/STRING/home.java
+++ striver_ninja/STRING/home.java
@@ -1,18 +1,14 @@
 public class home {
     public static void main(String[] args) {
-          String s = "madam";
-          String  str = "";
-          for(int i=s.length()-1;i>=0;i--)
+          String s = "Mohan is good boy";
+          int count =1;
+          for(int i=0;i<s.length()-1;i++)
           {
-               str = str + s.charAt(i);
-          }
-          System.out.println(str);
+            if(s.charAt(i) == ' ' && s.charAt(i+1) != ' '){
+                  count = count +1;
 
-          if(s.equals(str)){
-            System.out.println("it is palindrome ");
+            }
           }
-          else{
-            System.out.println("it is not palindrome");
-          }
+          System.out.println(count);
     }
 }
