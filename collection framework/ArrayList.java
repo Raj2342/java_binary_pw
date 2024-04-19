f5db00d0e3ad0070a1e316278b531a9c9978c35e
RAJ <technologys411@gmail.com>
2024-04-19 21:51:00 +0530

    arraylist

---
+++ collection framework/ArrayList.java
@@ -0,0 +1,8 @@
+import  java.util.*;
+public  class ArrayList {
+    public static void main(String[] args) {
+         ArrayList  al1 = new ArrayList();
+         al1.add(100);
+         
+    }
+}
