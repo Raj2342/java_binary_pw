a9d88983359370a4df65f8c8d0b5af35a642e0d5
RAJ <technologys411@gmail.com>
2024-04-22 19:38:09 +0530

    arraylist and linkedlist

---
+++ collection framework/LinkedList.java
@@ -0,0 +1,22 @@
+
+public class LinkedList {
+    public static void main(String[] args) {
+          
+        // LinkedList  ll1 = new LinkedList();
+        // ll1.add(100);
+        // ll1.add(200);
+        // ll1.add(300);
+        // System.out.println(ll1);
+        // LinkedList  ll2 = new LinkedList();
+        // ll2.add(1);
+        // ll2.add('d');
+        // ll2.add("bro");
+        // ll2.addFirst(17);
+        // ll2.addLast(18);
+        // System.out.println(ll2.peek());
+        // System.out.println(ll2);
+        // System.out.println(ll2.poll());
+        // System.out.println(ll2);
+
+    }
+}
---
+++ striver_ninja/STRING/anagram.java
@@ -0,0 +1,49 @@
+import java.lang.reflect.Array;
+
+public class anagram {
+    public static void main(String[] args) {
+         String s1 = "listen";
+         String s2 = "silent";
+        char s1arr[] =  s1.toCharArray();
+        char s2arr[] =  s2.toCharArray();
+        char temp1;
+        char temp2;
+         
+        //  sorting  of s1
+        for(int i=0;i<s1.length()-1;i++)
+        {
+            for(int j=0;j<s1.length()-1;j++)
+            {
+                if (s1arr[j]>s1arr[j+1]) {
+                    temp1 = s1arr[j+1];
+                    s1arr[j+1] = s1arr[j];
+                    s1arr[j] = temp1;
+
+                }
+            }
+        }
+        System.out.println();
+
+        // sort of s2
+        for(int i=0; i<s2.length()-1;i++)
+        {
+            for(int j=0;j<s2.length()-1;j++)
+            {
+                  if (s2arr[j] > s2arr[j+1]) {
+                      temp2 = s2arr[j+1];
+                      s2arr[j+1] = s2arr[j];
+                      s2arr[j]= temp2;
+
+                  }
+            }
+        }
+        System.out.println(s2arr);
+
+         if (s1arr == s2arr) {
+            System.out.println("it is anagram");
+         }
+         else{
+            System.out.println("it is not anagram");
+         }
+    }
+}
