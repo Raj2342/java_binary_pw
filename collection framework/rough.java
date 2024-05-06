0d77fd37cb1c250439acc498df8a68a472c5e605
RAJ <technologys411@gmail.com>
2024-05-06 14:19:46 +0530

    arraydeque, arraylist, linklist

---
+++ collection framework/rough.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class rough {
+    public static void main(String[] args) {
+         
+        ArrayList al1 = new ArrayList();
+        al1.add(100);
+        al1.add(200);
+
+
+    }
+}
--- striver_ninja/STRING/anagram/anagram_word.java
+++ striver_ninja/STRING/anagram/anagram_word.java
@@ -1,5 +1,5 @@
 public class anagram_word {
     public static void main(String[] args) {
-        
+        String st1 = S'
     }
 }
