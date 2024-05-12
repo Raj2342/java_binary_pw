a36a9c6c3ca1c243322d7529760f61e7f6778119
RAJ <technologys411@gmail.com>
2024-05-12 09:35:34 +0530

    sorted

---
+++ Api and Annotation/stream_api/sorted.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.*;
+import java.util.stream.*;
+public class sorted {
+     public static void main(String[] args) {
+         List<Integer>  s1 = Arrays.asList(1,2,3,4,5,6,7);
+         Stream<Integer> sortedValue = s1.stream()
+                      .filter(i->i%2==0)
+                      .sorted();
+         sortedValue.forEach(i->System.out.println(i));
+     }
+}
