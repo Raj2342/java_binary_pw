4063b75fea29ac0683c0f8858b5d4ab203939379
RAJ <technologys411@gmail.com>
2024-05-21 15:55:29 +0530

    duplicate array

--- dsa1/pw/duplicate_ele.java
+++ dsa1/pw/duplicate_ele.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class duplicate_ele {
     public static void main(String[] args) {
-          int 
+          int  arr[] = { 1,2,3,5,3,2,4,7,1,10};
+          Arrays.sort(arr);
+          for(int i=0;i<arr.length-1;i++)
+          {
+            if (arr[i]==arr[i+1]) {
+                System.out.println(arr[i]);
+             
+            }
+          }
+          
     }
 }
