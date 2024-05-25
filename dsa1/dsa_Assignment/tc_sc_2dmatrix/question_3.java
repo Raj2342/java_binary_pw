6173f14f11e5b1497ce9b9ed5612ed61e3c3c8dc
RAJ <technologys411@gmail.com>
2024-05-25 11:52:18 +0530

    print diagonal and anti-diagonal in 2d matrix

--- dsa1/dsa_Assignment/tc_sc_2dmatrix/question_3.java
+++ dsa1/dsa_Assignment/tc_sc_2dmatrix/question_3.java
@@ -1,7 +1,22 @@
 import java.util.Scanner;
 
 public class question_3 {
-    public static void BothDiagonal(int arr[][] , )
+    public static void BothDiagonal(int arr[][] ){
+         int m = arr.length-1;
+         int n= arr[0].length-1; 
+         for(int i=0;i<=m;i++)
+         {
+            for(int j=0; j<=n;j++)
+            {
+                 if (i==j ) {
+                    System.out.print(arr[i][j]+ " ");
+                 }
+                 else if (i+j==m){
+                    System.out.print(arr[i][j]+ " ");
+                 }
+            }
+         }
+    }
     public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
          System.out.println("ENTER ROW OF MATRIX ");
--- dsa1/dsa_Assignment/tc_sc_2dmatrix/rough.java
+++ dsa1/dsa_Assignment/tc_sc_2dmatrix/rough.java
@@ -1,49 +1,25 @@
 public class rough {
     public static void main(String[] args) {
-          int arr[][] = {
-              {1,2,3},
-              {4,5,6},
-              {7,8,9}
-          };
-        int m = arr.length-1;
-        
-        int n = arr[0].length-1;
-        // print below elment of diagonal 
-        for(int i=0;i<=m;i++)
-        {
-            for(int j=0;j<=n;j++)
-            { 
-                if (i<j) {
+        int arr[][] = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+      int m = arr.length-1;
+      
+      int n = arr[0].length-1;
+      
+      for(int i=0;i<=m;i++)
+         {
+            for(int j=0; j<=n;j++)
+            {
+                 if (i==j ) {
                     System.out.println(arr[i][j]);
-                }
+                 }
+                 else if (i+j==m){
+                    System.out.println(arr[i][j]);
+                 }
             }
-        }
-        // print  anti-diagonal 
-        // for(int i=0;i<=m;i++)
-        // {
-        //     System.out.println(arr[i][m-i]);
-        // }
-
-        // diagonal 
-        // for(int i=0;i<=m;i++)
-        // {
-        //     System.out.println(arr[i][i]);
-        // }
-
-         // print row and colomun 
-        // for(int j=0;j<=n;j++)
-        // {
-        //   System.out.print(" "+arr[2][j]);
-        //   // System.out.print("  "+arr[1][j]);
-        // }
-        
-        // print 2d matrix
-        // for(int i=0;i<m;i++)
-        // {
-        //   for(int j=0;j<n;j++)
-        //   {
-        //      System.out.println(arr[i][j]);
-        //   }
-        // }
+         }
     }
 }
