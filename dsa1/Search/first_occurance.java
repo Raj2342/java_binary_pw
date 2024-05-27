888c893248448b73b0d1d53c803c01ad9b246176
RAJ <technologys411@gmail.com>
2024-05-27 21:19:54 +0530

    first occurance element

--- dsa1/Search/first_occurance.java
+++ dsa1/Search/first_occurance.java
@@ -1,18 +1,30 @@
 public class first_occurance {
     public static void  main(String[] args) {
-          
-          int arr[] = { 2,4,7,7,9,11,12};
+          int result =0;
+          int arr[] = { 2,4,7,7,9,9,9};
+          int target = 9;
           int li=0;
           int hi = arr.length-1;
-          while (li<hi) {
+          while (li<=hi) {
             int mid = (li+hi)/2;
-            if (arr[mid]==7) {
+            if (arr[mid]==target) {
+               result = mid;
                 hi = mid-1;
-                System.out.println(hi);
             }
-            li++;
-            
+            else if(arr[mid]<target)
+            {
+              li= mid+1;
+            }
+            else if(arr[mid]>target)
+            {
+              hi= mid-1;
+            }
           }
+          
+          System.out.println(result);
+          
+            
+          
           
           
 
--- dsa1/Search/rough.java
+++ dsa1/Search/rough.java
@@ -1,15 +1,56 @@
+import java.util.*;
+import java.io.*;
 public class rough {
+
+     public static int first_occurance_print(int arr[] , int target)
+     {      
+            int result =0;
+            int li=0;
+            int hi=arr.length-1;
+
+            while (hi>=li) {
+               
+               int mid = (li+hi)/2;
+               
+
+                if(arr[mid]==target)
+                {
+                    result = mid;
+                    hi=mid-1; 
+                }
+                else if (arr[mid]<target) {
+                     li= mid+1;
+                }
+                else if (arr[mid]>target) {
+                    hi=mid-1;
+                }
+            }
+
+            return result;
+     }
     public static void main(String[] args) {
-         
-        int arr[] = { 2,4,7,7,9,11,12};
-         int li=0;
-         int hi = arr.length-1;
-         while (li<hi) {
-           int  midI = (li+hi)/2;
-           if (arr[midI]==7) {
-              System.out.println(midI);
-              break; oo           }
-            
-         }
+     
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Enter the size of array ");
+          int size = sc.nextInt();
+          System.out.println("Enter the target elemnt ");
+          int target = sc.nextInt();
+          System.out.println("Enter the array");
+          int arr[] = new int[size];
+          for(int i=0;i<size-1;i++)
+          {
+            arr[i]= sc.nextInt();
+          }
+
+        int store=  first_occurance_print(arr,target);
+        if (store==0) {
+            System.out.println("Not find first occurance element  ");
+        }
+        else{
+
+            System.out.println("first occurance element find "+store);
+        }
+
+
     }
 }
