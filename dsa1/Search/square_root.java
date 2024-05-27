7c461d62c40fbc04ca179799af1838a80a9dfb6a
RAJ <technologys411@gmail.com>
2024-05-27 23:36:56 +0530

    square root

--- dsa1/Search/square_root.java
+++ dsa1/Search/square_root.java
@@ -6,7 +6,7 @@ public class square_root {
         {
              int li=0;   
              int hi = target;
-             
+             int result =0;
 
              while (hi>=li) {
               int mid = (li+hi)/2;
@@ -15,13 +15,18 @@ public class square_root {
                    return mid; 
               }
               else if (val> target) {
+                // target ka  hamesa double value  hota hai 
+ // store the floor value, incase the number is not perfect square root, we can print the floor value
+
+                result=mid;
                 hi=mid-1;
               } 
               else if (val<target) {
+                
                 li=mid+1;
               } 
              }
-             return 0;
+             return result;
         }
     public static void main(String[] args) {
            
