c8c4578aeaf810e693134030f10c839a0894c45f
RAJ <technologys411@gmail.com>
2024-05-28 21:48:18 +0530

    bubble sort

--- dsa1/Sorting/buuble_sort/bubble_sort.java
+++ dsa1/Sorting/buuble_sort/bubble_sort.java
@@ -4,31 +4,32 @@ public class bubble_sort {
         
        public static void  play(int arr[] ) 
        {
-              int valStore ;
-              for(int i=0; i<arr.length; i++){
+               for(int i=0; i<arr.length; i++){
+                      boolean swapped = false;
                      for(int j=0; j<arr.length-i-1; j++){
                              if (arr[j]>arr[j+1]) {
-                                valStore = arr[j+1];
+                              int  temp = arr[j+1];
                                 arr[j+1]= arr[j];
-                                arr[j]= valStore;
+                                arr[j]= temp;
+
+                                swapped = true;
+
                              }
+                     }
+
+                     if (!swapped) {
+                        break; // go out of for loop 
                      }
                  } 
               
        }
        
-       
-
-       
-       
-       
        public static void main(String[] args) {
                   
               int arr[] = { 7,8,3,1,2 , 4 ,6 ,3 };
-              // System.out.println(play(arr));
-              
-        play(arr);
-//     System.out.println("Sorted Array in Ascending Order:");
+             //function calling  
+              play(arr);
+    System.out.println("Sorted Array in Ascending Order:");
     System.out.println(Arrays.toString(arr));
 
                 
