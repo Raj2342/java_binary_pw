ba7755179e99f10efe010459a968a544cdd195a5
RAJ <technologys411@gmail.com>
2024-05-31 10:21:04 +0530

    check even and odd

---
+++ dsa1/dsa_Assignment/number system/question_3.java
@@ -0,0 +1,22 @@
+// Q3. Problem 1: Given a number. Using bit manipulation, check whether it is odd or even.
+// Input 8, Even
+// 3, False 
+
+import java.util.Scanner;
+
+public class question_3 {
+    public static void main(String[] args) {
+            
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the number");
+            int  number = sc.nextInt();
+
+           if ((number & 1) ==1 ) {
+                System.out.println("odd");
+           }
+           else{
+            System.out.println("even");
+           }
+
+    }
+}
