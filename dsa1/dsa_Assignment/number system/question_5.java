4f3dbd3610b058813ec1c146d6cea4b1b11d6732
RAJ <technologys411@gmail.com>
2024-06-01 09:36:03 +0530

    duplicate element  find odd  appereance

---
+++ dsa1/dsa_Assignment/number system/question_5.java
@@ -0,0 +1,13 @@
+public class question_5 {
+    public static void main(String[] args) {
+        
+          int arr[]= {4,3,6,2,6,4,2,3,4};
+          int result = 0;
+          for(int i:arr){
+            result = result^i;
+
+          }
+          System.out.println(result);
+
+    }
+}
