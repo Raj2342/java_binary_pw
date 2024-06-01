1a04f716b678e19d69b8a418eec1bc9232e13341
RAJ <technologys411@gmail.com>
2024-06-01 10:16:06 +0530

    descending order using selection sort

--- dsa1/dsa_Assignment/soting_algorithm/question_2.java
+++ dsa1/dsa_Assignment/soting_algorithm/question_2.java
@@ -2,14 +2,35 @@
 // Input Array {3,5,1,6,0}
 // Output Array: {6, 5, 3, 1, 0}
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class question_2 {
 
       public static void  selection_sort(int arr[])
       {
-           
+            for(int i=0;i<arr.length-1;i++)
+            {
+                 int min = i;
+                for(int j=i+1;j<arr.length;j++)
+                {
+                if (arr[j]>arr[min]) {
+                    min= j;
+                }
+                }
+
+                if (min!=i) {
+                    int temp = arr[i];
+                    arr[i]=arr[min];
+                    arr[min]=temp;
+                    
+                }
+            }
       }
       public static void main(String[] args) {
             int arr[]= {3,5,1,6,0};
+            selection_sort(arr);
+            System.out.println(Arrays.toString(arr));
 
       }
 }
