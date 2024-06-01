4831ebcada03ead66eaf81ead284bab76855376a
RAJ <technologys411@gmail.com>
2024-06-01 10:27:42 +0530

    insertion sort  decreasing order

--- dsa1/dsa_Assignment/soting_algorithm/question_3.java
+++ dsa1/dsa_Assignment/soting_algorithm/question_3.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class question_3 {
+
+    public static void  insertion_sort(int arr[]){
+                 
+         for(int i=1;i<=arr.length-1;i++)
+         {
+            for(int j=i;j<=arr.length-1;j++)
+            {
+                while ( j>0 && arr[j-1]<arr[j]) {
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j]=temp;
+                    j--;
+                }
+            }
+         }
+
+    }
     public static void main(String[] args) {
-        
+         int arr[]= {3,5,1,6,0};
+            insertion_sort(arr);
+            System.out.println(Arrays.toString(arr));
     }
 }
