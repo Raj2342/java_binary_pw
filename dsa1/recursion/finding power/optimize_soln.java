245b21a1c9fee0b1b8e037024315509c57dac7e6
RAJ <technologys411@gmail.com>
2024-06-04 12:27:52 +0530

    optimize soluntion  power of element

--- dsa1/recursion/finding power/optimize_soln.java
+++ dsa1/recursion/finding power/optimize_soln.java
@@ -1,9 +1,23 @@
 import java.util.*;
 public class optimize_soln {
 
-    public static void power(int a,int b)
+    public static int power(int a,int b)
     {
-        
+      int result =0 ;
+      int  finalresult=0;
+        if(b==1){
+            return a;
+        }
+       else{
+        result = power(a,b/2);
+        finalresult = result*result;
+        if (b%2!=0) {
+            return a*finalresult;
+        }
+        else{
+            return finalresult;
+        }
+       }
     }
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
@@ -11,7 +25,8 @@ public class optimize_soln {
          int a =  sc.nextInt();
          System.out.println("Enter the value of b");
          int b =  sc.nextInt();
-         power(a,b);
+       int  final_result =  power(a,b);
+       System.out.println(final_result);
 
     }
 }
